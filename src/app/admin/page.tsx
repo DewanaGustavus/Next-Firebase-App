3fa27bd5b362738606af5f15c84268cb6cbac8b3
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2024-07-03 16:22:11 +0700

    Next set get firestore data

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,16 +1,56 @@
 'use client'
 import { useRouter } from "next/navigation";
 import { useAuthContext } from "../context/AuthContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import addData from "@/firebase/firestore/addData";
+import getData from "@/firebase/firestore/getData";
+import { DocumentData, DocumentSnapshot } from "firebase/firestore";
 
 export default function Page() {
     const { user } = useAuthContext()
     const router = useRouter()
+    const [data, setData] = useState<DocumentSnapshot<DocumentData, DocumentData> | null>();
+    const dataNumber = data?.data()?.number ?? -1
 
     useEffect(() => {
         if (user == null) router.push("/")
+        getData('users', 'user-id').then(
+            ({ result, error }) => {
+                if (error) {
+                    return console.log(error)
+                }
+                setData(result)
+                console.log("data", result)
+                return result
+            }
+        )
     }, [user])
 
-    return (<h1>Only logged in users can view this page</h1>);
+    const handleForm = async () => {
+        const data = {
+          number: Math.floor(Math.random() * 100)
+        }
+        const { result, error } = await addData('users', 'user-id', data)
+    
+        if (error) {
+          return console.log(error)
+        }
+
+        console.log("success", result)
+    }
+
+    return (
+    <>
+    <h1>
+        Only logged in users can view this page
+    </h1>
+    <div>
+        <h2>Your number: {dataNumber}</h2>
+        <button onClick={handleForm}>
+            set random number
+        </button>
+    </div>
+    </>
+    );
 }
 
